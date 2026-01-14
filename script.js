17b89fd36c6d383a408fc37d2b4e2119396af627
leamaevl <leamaev.l@gmail.com>
2026-01-15 00:37:24 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 window.addEventListener("load", () => {
   const reveal = document.getElementById("reveal");
 
+  if (!reveal) return;
+
   setTimeout(() => {
     reveal.classList.add("open");
   }, 300);
 
   setTimeout(() => {
-    reveal.remove();
+    reveal.style.display = "none";
   }, 2600);
 });
